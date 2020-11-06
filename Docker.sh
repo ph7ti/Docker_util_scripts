c03367536df1aea5180af971a50c82fe5bd23a8e
Raphael Matheus <raprock1@gmail.com>
2020-11-05 22:47:34 -0300

    Add files via upload
    Switcher para os serviços do Docker (tenho preguiça as vezes de usar o terminal pra isso)

---
+++ Docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+status=$(service docker status | grep active | awk '{print $2}')
+zenity --question --text "Status atual do Docker: $status - Deseja modificar?" --no-wrap --ok-label "Sim" --cancel-label "Não"
+opcao=$?
+if [[ $opcao == 0 ]]; then
+	if [[ $status = 'inactive' ]]; then
+		PASSWD=$(zenity --password --title="Authenticação" --text "$status")
+		echo $PASSWD | sudo service docker start
+	else
+		PASSWD=$(zenity --password --title="Authenticação" --text "$status")
+		echo $PASSWD | sudo service docker stop
+	fi
+	zenity --info --title="Informação" --text="Status atual do Docker: $(service docker status | grep active | awk '{print $2}')" --width="200" height="50"
+fi
